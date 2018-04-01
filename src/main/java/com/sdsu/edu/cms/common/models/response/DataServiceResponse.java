8a28ab5098e3d159f8275d1cbac3a5cd73823c15
pavanpkp33 <pavanpkp33@gmail.com>
2018-03-31 21:45:26 -0700

    Bug fix

---
+++ src/main/java/com/sdsu/edu/cms/common/models/response/DataServiceResponse.java
@@ -0,0 +1,34 @@
+package com.sdsu.edu.cms.common.models.response;
+
+
+import java.util.List;
+
+public class DataServiceResponse {
+
+    private List<Object> data;
+    private String message;
+
+    public DataServiceResponse(List<Object> data, String message) {
+        this.data = data;
+        this.message = message;
+    }
+
+    public DataServiceResponse() {
+    }
+
+    public List<Object> getData() {
+        return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setData(List<Object> data) {
+        this.data = data;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/sdsu/edu/cms/common/models/user/AuthUser.java
@@ -0,0 +1,41 @@
+package com.sdsu.edu.cms.common.models.user;
+
+public class AuthUser {
+
+    private String id;
+    private String email;
+    private String password;
+
+    public AuthUser() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthUser(String id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+}
